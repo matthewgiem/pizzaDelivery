255a45df0ae673fcb005b76f3ab7ff029939a340
Matthew Giem <matthew.giem@gmail.com>
2016-06-17 13:48:20 -0700

    create the functionality for storing a pricing 1 type of pizza with differing number of topings, size, and number of pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -3,67 +3,66 @@ $(document).ready(function()  {
   $("#submitOrder").click(function()  {
     var firstName = $("#firstName").val();
     var lastName = $("#lastName").val();
-    var pizzaNumber = $(".pizzaNumber").val();
+    var pizzaNumber = parseInt($(".pizzaNumber").val());
     var toppings = [];
+    var pizzaSize = parseInt($("input[name='size']:checked").val());
     $("input:checkbox[name=toppings]:checked").each(function(){
       toppings.push($(this).val());
     });
-    var customersPizzaOrder = new PizzaOrder(firstName, lastName, pizzaNumber, toppings)
-    console.log(firstName);
-    console.log(lastName);
-    console.log(pizzaNumber);
-    console.log(toppings);
-    console.log(customersPizzaOrder);
+    var customersPizzaOrder = new PizzaOrder(firstName, lastName, pizzaNumber, toppings, pizzaSize)
+    $("#finalPrice").text(customersPizzaOrder.pizzaPrice(toppings));
     event.preventDefault();
   });
+  event.preventDefault();
 })
 
 
 
 // Back End Logic
 
-function PizzaOrder (customerFirst, customerLast, pizzaNumber, toppings) {
+function PizzaOrder (customerFirst, customerLast, pizzaNumber, toppings, pizzaSize) {
   this.firstName = customerFirst;
   this.lastName = customerLast;
-  this.numberOfPizzas = pizzaNumber
+  this.numberOfPizzas = pizzaNumber;
   this.pizzaToppings = [toppings];
+  this.pizzaSize = pizzaSize;
 }
 
 
-PizzaOrder.prototype.pizzaPrice = function () {
-  //price of the pizza
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function  Address(street, city, state)  {
-  this.street = street;
-  this.city = city;
-  this.state  = state;
+PizzaOrder.prototype.pizzaPrice = function (topping) {
+  return this.numberOfPizzas * ((topping.length) * 1 + this.pizzaSize);
 }
 
 
 
 
-PizzaOrder.prototype.fullAddress = function () {
-  return this.street + ", " + this.city + ", " + this.state;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function  Address(street, city, state)  {
+//   this.street = street;
+//   this.city = city;
+//   this.state  = state;
+// }
+//
+//
+//
+//
+// PizzaOrder.prototype.fullAddress = function () {
+//   return this.street + ", " + this.city + ", " + this.state;
+// }
